fd613eb2c17d02938e44312387eaa653886645e8
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2020-10-19 15:18:35 +0900

    hide HASHMAP

--- src/lang.rs
+++ src/lang.rs
@@ -1,20 +1,20 @@
 pub struct Lang(pub String);
 
 use std::collections::HashMap;
-lazy_static! {
-    static ref HASHMAP: HashMap<String, String> = include_str!("../config/links.tsv")
-        .lines()
-        .collect::<Vec<_>>()
-        .iter()
-        .map(|line| {
-            let v: Vec<&'static str> = line.splitn(2, "\t").collect();
-            (v[0].to_owned(), v[1].to_owned())
-        })
-        .collect::<HashMap<_, _>>();
-}
 
 impl Lang {
     pub fn url(&self) -> String {
+        lazy_static! {
+            static ref HASHMAP: HashMap<String, String> = include_str!("../config/links.tsv")
+                .lines()
+                .collect::<Vec<_>>()
+                .iter()
+                .map(|line| {
+                    let v: Vec<&'static str> = line.splitn(2, "\t").collect();
+                    (v[0].to_owned(), v[1].to_owned())
+                })
+                .collect::<HashMap<_, _>>();
+        }
         match HASHMAP.get(&self.0) {
             Some(u) => u.to_owned(),
             None => panic!("Unknown language name {}", self.0)
